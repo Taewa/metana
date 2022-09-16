8aabffa83ee26ce78ac3d9015313b98e1bee4e98
Taehwa KIM <kim.taehwa@ing.com>
2022-09-16 11:42:37 +0200

    refactor: number format

--- w1-erc20/contracts/Sanctions.sol
+++ w1-erc20/contracts/Sanctions.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Sanctions is ERC20 {
     constructor() ERC20("AuthToken", "AUT") {
-        _mint(msg.sender, 1000000 * 10 ** decimals());
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
         authority = msg.sender;
     }
 
