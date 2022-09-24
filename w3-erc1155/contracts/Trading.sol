24f4f688d2a368cdfd251786aa4649d853189b8d
Taehwa KIM <kim.taehwa@ing.com>
2022-09-24 15:28:27 +0200

    feat(ERC1155): init Trading contract and basic mining rules

---
+++ w3-erc1155/contracts/Trading.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+/**
+You must have a total of 7 tokens within the collection id [0-6]
+There is no supply limit for each token
+Anyone can mint tokens [0-2], but there is a 1-minute cooldown between mints. These are free to mint except for the gas cost.
+Token 3 can be minted by burning token 0 and 1.
+Token 4 can be minted by burning token 1 and 2
+Token 5 can be minted by burning 0 and 2
+Token 6 can be minted by burning 0, 1, and 2
+Tokens [3-6] cannot be forged into other tokens
+Tokens [3-6] can be burned but you get nothing back
+You can trade any token for [0-2] by hitting the trade this button.
+The process of burning and minting is called forging in this context.
+The webapp must tell the user how much matic they have (we will use the polygon network for cost savings)
+The webapp must tell the user how much of each token they have
+Provide a link to the OpenSea page somewhere
+Important if the website detects someone is not on the polygon network, it must prompt them to change and autofill the feeds for changing the network (lesson on this later)
+Important please use some styling on this website to make it look nice (bootstrap, tailwind CSS, etc). This is something you can show to future employers or business partners.
+You must use 2 separate contracts. One for the ERC1155 token, and one for the forging logic. The forging logic will need mint privileges.
+ */
+contract Trading is ERC1155 {
+    uint256 public constant TOKEN_AMOUNT = 100_000_000 * 10 **18;
+
+    struct Pokemon {
+        uint256 id;
+        string name;
+        bool isFree;
+        uint256[] requirement;
+    }
+
+    mapping (uint256 => Pokemon) public PokemonList;
+    mapping (uint256 => uint256) public cooldownList;
+
+    constructor() ERC1155("https://game.example/api/item/{id}.json") {
+        init();
+    }
+
+    function mint(uint256 _tokenId) public {
+        if(PokemonList[_tokenId].isFree) {
+            _mint(msg.sender, _tokenId, 1, "");
+        } else {
+            mintNonFreeToken(_tokenId);
+        }
+    }
+
+    function mintNonFreeToken(uint256 _tokenId) internal {
+        uint256[] memory requirementTokens = PokemonList[_tokenId].requirement;
+        uint256[] memory amounts = new uint256[](requirementTokens.length);
+     
+        for(uint i = 0; i < requirementTokens.length; i++) {
+            amounts[i] = 1;
+        }
+
+        burnBatch(requirementTokens, amounts);
+        _mint(msg.sender, _tokenId, 1, "");
+    }
+
+    function burnBatch(uint256[] memory _tokenIds, uint256[] memory amounts) internal {
+        _burnBatch(msg.sender, _tokenIds, amounts);
+    }
+
+    function cooldownMinting() internal {}
+
+    function init() private {
+        PokemonList[0].id = 0;
+        PokemonList[0].name = 'SCIZOR';
+        PokemonList[0].isFree = true;
+        
+        PokemonList[1].id = 1;
+        PokemonList[1].name = 'LUCARIO';
+        PokemonList[1].isFree = true;
+        
+        PokemonList[2].id = 2;
+        PokemonList[2].name = 'MUDKIP';
+        PokemonList[2].isFree = true;
+
+        PokemonList[3].id = 3;
+        PokemonList[3].name = 'PIKACHU';
+        PokemonList[3].isFree = false;
+        PokemonList[3].requirement = [0, 1];
+        
+        PokemonList[4].id = 4;
+        PokemonList[4].name = 'SQUIRTLE';
+        PokemonList[4].isFree = false;
+        PokemonList[4].requirement = [1, 2];
+        
+        PokemonList[5].id = 5;
+        PokemonList[5].name = 'UMBREON';
+        PokemonList[5].isFree = false;
+        PokemonList[5].requirement = [0, 2];
+        
+        PokemonList[6].id = 6;
+        PokemonList[6].name = 'CHARIZARD';
+        PokemonList[6].isFree = false;
+        PokemonList[6].requirement = [0, 1, 2];
+
+    }
+}
